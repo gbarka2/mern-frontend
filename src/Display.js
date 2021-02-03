9eeaa676034fa97de77ba93d273081134d4d7011
Gary Barkauski <gbarka2@gmail.com>
2021-02-02 19:54:34 -0700

    Complete Cars map in Display

--- src/Display.js
+++ src/Display.js
@@ -1,13 +1,31 @@
 import React from 'react'
 
 const Display = (props) => {
-  console.log(props.cars)
+  const {cars} = props
+  // console.log(cars)
+  const loaded = () => {
+      return (
+        <div style={{textAlign: "center"}}>DISPLAY
+        {cars.map(car => {
+          return(
+            <article key={car._id}>
+              <h1>{car.year} {car.make} {car.model}</h1>
+              <div>
+                <button>Edit Listing</button>
+                <button>Sold!</button>
+              </div>
+            </article>
+          )
+        })}
+      </div>
+    )
+  }
 
-  return (
-    <div>DISPLAY
+  const loading = () => {
+    return <h1>Vroom Vroom...</h1>
+  }
 
-    </div>
-  )
+  return cars.length > 0 ? loaded() : loading()
+
 }
-
 export default Display
